9a27fb9020b8857f924a660e9ef2436e05de7068
Nicolas <berdunicolas@gmail.com>
2025-05-16 02:11:38 -0300

    subdominio admin

--- routes/auth.php
+++ routes/auth.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-});
 
+Route::domain('admin.'. config('app.url'))->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+        Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+    });
+    
+    
+    Route::middleware('auth')->group(function () {
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout'); 
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout'); 
 });
