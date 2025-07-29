7c0fb78ac2906e9d9785bda9256ba8901987b733
Nicolas <berdunicolas@gmail.com>
2025-07-29 06:04:16 -0300

    Fix links footer

--- resources/views/invitations/modules/foot.blade.php
+++ resources/views/invitations/modules/foot.blade.php
@@ -1,7 +1,7 @@
 @if ($module->invitation->seller->has_banner)
     <section class="salon" style="background-color:#454545; background-image: url('{{$module->invitation->seller->media('banner_bg')->first()?->getMediaUrl()}}');">
         <div class="item">
-            <div class="left"><img src="{{$module->invitation->seller->media('banner_logo')->first()?->getMediaUrl()}}" alt="Event Link"/></div>
+            <div class="left" target="_blank"><img src="{{$module->invitation->seller->media('banner_logo')->first()?->getMediaUrl()}}" alt="Event Link"/></div>
             @if(!$module->invitation->seller->only_logo)
                 <div class="right">
                     <p>{!!$module->invitation->seller->text!!}</p>
@@ -9,19 +9,19 @@
                         <a href="https://{{$module->invitation->seller->ig_link}}" target="_blank"><i class="fa-brands fa-instagram"></i></a>
                     @endempty
                     @empty(!$module->invitation->seller->wpp_link)
-                        <a href="https://{{$module->invitation->seller->wwp_link}}"><i class="fa-brands fa-whatsapp"></i></a>
+                        <a href="https://{{$module->invitation->seller->wwp_link}}" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
                     @endempty
                     @empty(!$module->invitation->seller->tk_link)
-                        <a href="https://{{$module->invitation->seller->tk_link}}"><i class="fa-brands fa-tiktok"></i></a>
+                        <a href="https://{{$module->invitation->seller->tk_link}}" target="_blank"><i class="fa-brands fa-tiktok"></i></a>
                     @endempty
                     @empty(!$module->invitation->seller->x_link)
-                        <a href="https://{{$module->invitation->seller->x_link}}"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="https://{{$module->invitation->seller->x_link}}" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
                     @endempty
                     @empty(!$module->invitation->seller->ytube_link)
-                        <a href="https://{{$module->invitation->seller->ytube_link}}"><i class="fa-brands fa-youtube"></i></a>
+                        <a href="https://{{$module->invitation->seller->ytube_link}}" target="_blank"><i class="fa-brands fa-youtube"></i></a>
                     @endempty
                     @empty(!$module->invitation->seller->site_link)
-                        <a href="https://{{$module->invitation->seller->site_link}}"><i class="fa-regular fa-link"></i></a>
+                        <a href="https://{{$module->invitation->seller->site_link}}" target="_blank"><i class="fa-regular fa-link"></i></a>
                     @endempty
                 </div>
             @endif
